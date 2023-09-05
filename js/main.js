a5c71c14c2d8e83810c4b7c998cad8d1c8f46b3f
samandarreactjs <samandarprogrammer002@gmail.com>
2023-09-06 02:54:07 +0500

    collab

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// const user = document.getElementById('user');
+// const user_btn = document.getElementById('btn');
+// const username = document.getElementById('username')
+
+// function addLocal() {
+//     localStorage.setItem('name', String(user.value));
+//     location.href = './sign.html';
+// }
+
+// const checkName = localStorage.getItem('name')
+
+// function checkUser() {
+//     if (checkName === String(username.value)) {
+//         location.href = './index.html'
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+
+const user = document.getElementById('user');
+const user_btn = document.getElementById('btn-1');
+const username = document.getElementById('username');
+
+function addLocal() {
+    localStorage.setItem('name', String(user.value));
+    location.href = './sign.html';
+}
+
+const checkName = localStorage.getItem('name');
+
+function checkName()  {
+    if(checkName === String(username.value)) {
+        location.href = './index.html'
+    } else {
+        console.log(false);
+    }
+}
